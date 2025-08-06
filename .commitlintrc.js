64858e980e288f127814d976ee3716278d6f8094
Eyar Yanay <eyareyar123@gmail.com>
2025-08-06 15:04:21 +0300

    [DOCS] - added decription to the commit enforcement

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,21 @@
-// .commitlintrc.js
+/**
+ * Commit Message Format: [TYPE] - message
+ *
+ * Allowed types:
+ * [FEAT]     - A new feature or significant enhancement
+ * [FIX]      - A bug fix or error correction
+ * [STYLE]    - Code style changes (formatting, linting, etc.)
+ * [REFACTOR] - Code restructuring without changing behavior
+ * [DOCS]     - Documentation updates or additions
+ * [TEST]     - Adding or updating tests
+ * [CHORE]    - Maintenance tasks (e.g. dependency updates)
+ * [PERF]     - Performance improvements
+ * [CI]       - Changes to CI/CD configuration
+ * [BUILD]    - Changes to build system or tooling
+ *
+ * Format is strictly enforced using Commitlint + Husky.
+ * Example: [FIX] - Correct login bug
+ */
 module.exports = {
   parserPreset: {
     parserOpts: {
