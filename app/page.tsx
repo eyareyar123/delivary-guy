9ed21059194a438ca175beccd5f7bddd4bf9f8b7
Eyar Yanay <eyareyar123@gmail.com>
2025-08-11 23:05:49 +0300

    [FEAT] - styled the main page - added responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -29,18 +29,21 @@ export default function Home() {
       >
         <DarkModeToggle />
 
-        <main className="min-h-screen p-20 bg-white dark:bg-gray-900 dark:text-white transition-colors duration-300">
-          <main className="flex items-center justify-center">
+        <main className="flex flex-col h-screen p-20 bg-white dark:bg-gray-900 dark:text-white transition-colors duration-300 gap-y-6">
+          <header className="flex items-center justify-center">
             <h1 className="text-4xl font-bold">Delivery Line Manager</h1>
-          </main>
+          </header>
+
           <Map deliveryPoints={deliveryPoints} />
 
-          <DeliveryForm onAdd={handleAddPoint} />
+          <div className="flex justify-between items-center w-full max-h-100 ">
+            <DeliveryForm onAdd={handleAddPoint} />
 
-          <DeliveryList
-            deliveryPoints={deliveryPoints}
-            onDelete={handleDeletePoint}
-          />
+            <DeliveryList
+              deliveryPoints={deliveryPoints}
+              onDelete={handleDeletePoint}
+            />
+          </div>
         </main>
       </LoadScript>
     </>
